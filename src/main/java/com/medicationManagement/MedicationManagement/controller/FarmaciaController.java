ed17f6b96ad10f8656730c408274c8f96ce16e66
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-28 18:36:30 -0300

    cria metodo no buscar por cnpj no farmaciaController

--- src/main/java/com/medicationManagement/MedicationManagement/controller/FarmaciaController.java
+++ src/main/java/com/medicationManagement/MedicationManagement/controller/FarmaciaController.java
@@ -5,6 +5,7 @@ import com.medicationManagement.MedicationManagement.service.FarmaciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,16 @@ public class FarmaciaController {
     public ResponseEntity<List<Farmacia>> buscarTodasFarmacias() {
         List<Farmacia> farmacias = farmaciaService.listarTodasFarmacias();
         return ResponseEntity.ok(farmacias);
+    }
+
+    @GetMapping("/{cnpj}")
+    public ResponseEntity<?> buscarFarmaciaPorCnpj(@PathVariable Long cnpj) {
+        Farmacia farmacia = farmaciaService.obterFarmaciaPorCnpj(cnpj);
+
+        if (farmacia != null) {
+            return ResponseEntity.ok(farmacia);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
