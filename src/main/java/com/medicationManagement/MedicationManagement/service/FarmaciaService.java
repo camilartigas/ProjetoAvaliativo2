b2da1d8607ed6b48abbe67571e915e31464158bd
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-28 18:18:49 -0300

    cria farmaciaService

---
+++ src/main/java/com/medicationManagement/MedicationManagement/service/FarmaciaService.java
@@ -0,0 +1,24 @@
+package com.medicationManagement.MedicationManagement.service;
+
+import com.medicationManagement.MedicationManagement.model.Farmacia;
+import com.medicationManagement.MedicationManagement.repository.FarmaciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class FarmaciaService {
+
+    private final FarmaciaRepository farmaciaRepository;
+
+    @Autowired
+    public FarmaciaService(FarmaciaRepository farmaciaRepository) {
+        this.farmaciaRepository = farmaciaRepository;
+    }
+
+    public List<Farmacia> listarTodasFarmacias() {
+        return farmaciaRepository.findAll();
+    }
+}
