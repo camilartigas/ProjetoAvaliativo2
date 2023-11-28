cb6bf8b9ef0e613fc3bda3e8bdddbc450a472854
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-28 19:27:10 -0300

    lombok farmacia

--- src/main/java/com/medicationManagement/MedicationManagement/model/Farmacia.java
+++ src/main/java/com/medicationManagement/MedicationManagement/model/Farmacia.java
@@ -1,12 +1,18 @@
 package com.medicationManagement.MedicationManagement.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "FARMACIAS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Farmacia {
 
-    @Id //chave primária
+    @Id // Chave primária
     private Long cnpj;
 
     @Column(nullable = false)
@@ -25,78 +31,4 @@ public class Farmacia {
 
     @Embedded
     private Endereco endereco;
-
-
-    //construtores
-    public Farmacia() {
-    }
-
-    public Farmacia(Long cnpj, String razaoSocial, String nomeFantasia, String email, String telefone, String celular, Endereco endereco) {
-        this.cnpj = cnpj;
-        this.razaoSocial = razaoSocial;
-        this.nomeFantasia = nomeFantasia;
-        this.email = email;
-        this.telefone = telefone;
-        this.celular = celular;
-        this.endereco = endereco;
-    }
-
-
-
-    // Getters e Setters
-    public Long getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(Long cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getRazaoSocial() {
-        return razaoSocial;
-    }
-
-    public void setRazaoSocial(String razaoSocial) {
-        this.razaoSocial = razaoSocial;
-    }
-
-    public String getNomeFantasia() {
-        return nomeFantasia;
-    }
-
-    public void setNomeFantasia(String nomeFantasia) {
-        this.nomeFantasia = nomeFantasia;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
 }
