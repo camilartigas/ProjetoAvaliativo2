3c6ad7615c69097dce522d91dd202c5b429e448a
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-28 19:56:30 -0300

    lombok medicamento

--- src/main/java/com/medicationManagement/MedicationManagement/model/Medicamento.java
+++ src/main/java/com/medicationManagement/MedicationManagement/model/Medicamento.java
@@ -1,7 +1,13 @@
 package com.medicationManagement.MedicationManagement.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "MEDICAMENTOS")
 public class Medicamento {
@@ -26,79 +32,4 @@ public class Medicamento {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TipoMedicamento tipoMedicamento;
-
-
-
-    // Construtores
-    public Medicamento() {
-    }
-
-    public Medicamento(Integer numeroRegistro, String nome, String laboratorio, String dosagem, String descricao, Float preco, TipoMedicamento tipoMedicamento) {
-        this.numeroRegistro = numeroRegistro;
-        this.nome = nome;
-        this.laboratorio = laboratorio;
-        this.dosagem = dosagem;
-        this.descricao = descricao;
-        this.preco = preco;
-        this.tipoMedicamento = tipoMedicamento;
-    }
-
-
-    // Getters e Setters
-
-    public Integer getNumeroRegistro() {
-        return numeroRegistro;
-    }
-
-    public void setNumeroRegistro(Integer numeroRegistro) {
-        this.numeroRegistro = numeroRegistro;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLaboratorio() {
-        return laboratorio;
-    }
-
-    public void setLaboratorio(String laboratorio) {
-        this.laboratorio = laboratorio;
-    }
-
-    public String getDosagem() {
-        return dosagem;
-    }
-
-    public void setDosagem(String dosagem) {
-        this.dosagem = dosagem;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Float getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Float preco) {
-        this.preco = preco;
-    }
-
-    public TipoMedicamento getTipoMedicamento() {
-        return tipoMedicamento;
-    }
-
-    public void setTipoMedicamento(TipoMedicamento tipoMedicamento) {
-        this.tipoMedicamento = tipoMedicamento;
-    }
 }
