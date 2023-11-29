1743b23214f78e5d42392791f3f278ab348e6354
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-28 21:11:04 -0300

    cria farmaciaRequest

---
+++ src/main/java/com/medicationManagement/MedicationManagement/model/dto/FarmaciaRequest.java
@@ -0,0 +1,14 @@
+package com.medicationManagement.MedicationManagement.model.dto;
+
+import lombok.Data;
+
+@Data
+public class FarmaciaRequest {
+    private Long cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String email;
+    private String telefone;
+    private String celular;
+    private EnderecoRequest endereco;
+}
