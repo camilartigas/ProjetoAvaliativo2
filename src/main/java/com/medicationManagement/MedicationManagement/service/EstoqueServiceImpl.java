b7ef5b563702c014caac710c316228aa7e0de49b
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-12-01 05:41:37 -0300

    cria estoqueServiceImpl

---
+++ src/main/java/com/medicationManagement/MedicationManagement/service/EstoqueServiceImpl.java
@@ -0,0 +1,18 @@
+package com.medicationManagement.MedicationManagement.service;
+
+import com.medicationManagement.MedicationManagement.dto.EstoqueRequest;
+import com.medicationManagement.MedicationManagement.dto.EstoqueResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstoqueServiceImpl implements EstoqueService {
+
+    // Implementar a lógica para adicionar/atualizar o medicamento no estoque
+    @Override
+    public EstoqueResponse adicionarMedicamentoAoEstoque(EstoqueRequest estoqueRequest) {
+        // Lógica para adicionar/atualizar o medicamento no estoque da farmácia
+        // ...
+        return null; // Retornar os dados atualizados do estoque
+    }
+}
+// (Implementação do serviço para adição de medicamentos ao estoque)
